03c626db67c47c99da6c112b8dc8f43d123e085d
Ben Smith <ben@barxsmith.sbs>
2025-06-28 23:38:37 +0100

    Initial Commit

---
+++ OWUITests/OWUITests.swift
@@ -0,0 +1,16 @@
+//
+//  OWUITests.swift
+//  OWUITests
+//
+//  Created by Ben Smith on 28/06/2025.
+//
+
+import Testing
+
+struct OWUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
