34d673412527d0fd6bae503be08deb1c01dcfbdf
Ben Smith <ben@barxsmith.sbs>
2025-06-29 21:08:46 +0100

    3

--- OWUI/ContentView.swift
+++ OWUI/ContentView.swift
@@ -28,16 +28,34 @@ struct WebView: NSViewRepresentable {
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
+    @State private var urlInput: String = ""
+    
+    
+
+    // Get owuiURL from the saved data in userdefaults
+    private let owuiURL = UserDefaults.standard.string(forKey: "owuiURL") ?? "pineapple"
 
     var body: some View {
+        if owuiURL == "pineapple" || owuiURL == "https://example.com" {
+            VStack {
+                // Input box for the OWUI URL
+                TextField("http://localhost:8080", text: $urlInput)
+                    .frame(maxWidth: 300)
+                
+                Button("Set URL") {
+                    UserDefaults.standard.set(urlInput, forKey: "owuiURL")
+                }
+                
+            } .frame(minWidth: 600, minHeight: 300)
+        } else {
         VStack {
-            if let url = URL(string: "https://ai.benjs.uk") {
+            if let url = URL(string: owuiURL) {
                 WebView(url: url)
-                    .frame(minWidth: 400, minHeight: 300)
+                    .frame(minWidth: 600, minHeight: 300)
             } else {
                 Text("Invalid URL")
             }
-        }
+        }}
     }
 
     private func addItem() {
